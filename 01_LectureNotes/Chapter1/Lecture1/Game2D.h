a3cbed33f8238a180e265e570a4663412c3e7fad
yubeomyeol@gmail.com <yubeomyeol@gmail.com>
2020-03-24 15:42:34 +0900

    Lecture 1.1

--- 01_LectureNotes/Chapter1/Lecture1/Game2D.h
+++ 01_LectureNotes/Chapter1/Lecture1/Game2D.h
@@ -30,25 +30,27 @@ namespace jm
 
 		Timer timer;
 
-		float spf = 1.0f / 60.0f;		 // second(s) per frame
+		float spf = 1.0f / 60.0f;		 // second(s) per frame; fps의 역수
 
 		// control options
 		std::map<int, bool> key_status;  // key_id, is_pressed
 		std::map<int, bool> mbtn_status; // mouse_button_id, is_pressed
-		bool draw_grid = false;
+		bool draw_grid = true;           // draw grid or not
 
 	public:
 		Game2D()
 		{}
 
+		// Setting; full screen mode, the number of display
 		Game2D(const std::string& _title, const int& _width, const int& _height,
 			const bool & use_full_screen = false, const int & display_ix = 0);
-
+		
 		~Game2D();
 
 		Game2D & init(const std::string& _title, const int& _width, const int& _height,
 			const bool & use_full_screen = false, const int & display_ix = 0);
 
+		// Error reporting function
 		void reportErrorAndExit(const std::string& function_name, const std::string& message);
 
 		bool isKeyPressed(const int& key);
@@ -61,14 +63,14 @@ namespace jm
 
 		vec2 getCursorPos(const bool& screen_coordinates = true);
 
-		float getTimeStep();
+		float getTimeStep(); // 한 프레임 당 시간이 얼마나 걸리는지 측정해 주는 func.
 
 		void drawGrid();
 
-		void run();
+		void run(); // 의 while이 main loop
 
-		virtual void update() 
-		{
+		virtual void update() // void run();내의 update();가 실행될 때 override됨 
+		{                     // 하나의 virtual func.만 사용하여 구조를 파악한다
 			// draw
 			// play sould
 			// physics update
